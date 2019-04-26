35863894430faa35b516108ed45311c8eb6df94e
Edward Fornieles <42042091+EdwardFornieles@users.noreply.github.com>
2019-04-26 01:10:38 +0100

    2

--- examples/aframe/heineken/index.js
+++ examples/aframe/heineken/index.js
@@ -19,6 +19,8 @@ AFRAME.registerComponent('artgalleryframe', {
     frameEl.setAttribute('gltf-model', '#heineken')
     frameEl.setAttribute('rotation', '90 0 00')
     frameEl.setAttribute('animation-mixer', 'clip: *; loop: false')
+    frameEl.setAttribute('geometry', 'primitive: box')
+    frameEl.setAttribute('material', 'src: #hnkmap01')
 
     if (this.data.rotated) {
       // Rotate the frame for a landscape target
