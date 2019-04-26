125d93012fff1ac77708dc89c7d1ac10a15a5b7c
Edward Fornieles <42042091+EdwardFornieles@users.noreply.github.com>
2019-04-26 01:35:16 +0100

    2

--- examples/aframe/heineken/index.js
+++ examples/aframe/heineken/index.js
@@ -18,8 +18,7 @@ AFRAME.registerComponent('artgalleryframe', {
     frameEl.setAttribute('position', '0 0 0')
     frameEl.setAttribute('gltf-model', '#heineken')
     frameEl.setAttribute('rotation', '90 0 00')
-    frameEl.setAttribute('animation-mixer', 'clip: *')
-    frameEl.setAttribute('animation', 'loop: false')
+    frameEl.setAttribute('animation', 'clip: *')
     frameEl.setAttribute('geometry', 'primitive: box')
     frameEl.setAttribute('material', 'src: #hnkmap01')
 
