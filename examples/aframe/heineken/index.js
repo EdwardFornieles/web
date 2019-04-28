51e1c7bfcdc240bc882be2bb6c1d6445f1508ac5
Edward Fornieles <42042091+EdwardFornieles@users.noreply.github.com>
2019-04-28 05:28:58 +0100

    Update index.js

--- examples/aframe/heineken/index.js
+++ examples/aframe/heineken/index.js
@@ -14,10 +14,10 @@ AFRAME.registerComponent('heinekenframe', {
     object3D.visible = false
 
     const frameEl = document.createElement('a-entity')    
-    frameEl.setAttribute('gltf-model', '#heineken')
     frameEl.setAttribute('scale', '0.1 0.1 0.1')
     frameEl.setAttribute('position', '0 0 0')
     frameEl.setAttribute('rotation', '90 0 0')
+    frameEl.setAttribute('gltf-model', '#heineken')
 //    frameEl.setAttribute('animation', 'clip: *')
     frameEl.setAttribute('material', 'color: lightpink; metalness:0.8; roughness:0.3; sphericalEnvMap: #sky; envMapIntensity: 1.0; normalMap: #normal; normalScale: 1 -1; ambientOcclusionMapIntensity: 1.0; ambientOcclusionMap: #ao; displacementMap: #displacement; displacementScale: 2.436143; displacementBias: -0.428408;')
 
