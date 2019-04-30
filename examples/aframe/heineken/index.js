cc7b4d86d70fa52abb76f3df6bc1b57de8adcb0d
Edward Fornieles <42042091+EdwardFornieles@users.noreply.github.com>
2019-04-30 06:36:20 +0100

    2

--- examples/aframe/heineken/index.js
+++ examples/aframe/heineken/index.js
@@ -7,6 +7,9 @@ AFRAME.registerComponent('heinekenframe', {
   init: function () {
     const contents = document.getElementById('contents')
     const container = document.getElementById('container')
+    const image = document.getElementById('photoModeImage')
+    const shutterButton = document.getElementById('shutterButton')
+    const closeButton = document.getElementById('closeButton')
 
     const {object3D, sceneEl} = this.el
 
@@ -69,6 +72,35 @@ AFRAME.registerComponent('heinekenframe', {
     showhide(false, 'bottom');
 
     console.log("loaded");
+
+        closeButton.addEventListener('click', () => {
+      container.classList.remove('photo')
+    })
+
+    shutterButton.addEventListener('click', () => {
+      // Emit a screenshotrequest to the xrweb component
+      this.el.sceneEl.emit('screenshotrequest')
+
+      // Show the flash while the image is being taken
+      container.classList.add('flash')
+    })
+
+    this.el.sceneEl.addEventListener('screenshotready', e => {
+      // Hide the flash
+      container.classList.remove('flash')
+
+      // If an error occurs while trying to take the screenshot, e.detail will be empty.
+      // We could either retry or return control to the user
+      if (!e.detail) {
+        return
+      }
+
+      // e.detail is the base64 representation of the JPEG screenshot
+      image.src = 'data:image/jpeg;base64,' + e.detail
+
+      // Show the photo
+      container.classList.add('photo')
+    })
   }
 })
 
