4f989ca58a0e47bfc888e49579de1e57d9679789
Edward Fornieles <42042091+EdwardFornieles@users.noreply.github.com>
2019-05-06 02:25:57 +0100

    Update index.js

--- examples/aframe/heineken/index.js
+++ examples/aframe/heineken/index.js
@@ -106,8 +106,10 @@ AFRAME.registerComponent('heinekenframe', {
         object3D.position.copy(detail.position)
         //object3D.position.y = 0;
         object3D.quaternion.copy(detail.rotation)
-        object3D.scale.set(detail.scale * 1.5, detail.scale * 1.5, detail.scale * 1.5)
+        //object3D.scale.set(detail.scale * 1.5, detail.scale * 1.5, detail.scale * 1.5)
 
+        console.log(object3D.position);
+        
         oldPos = oldPos + 1;
       }
 
